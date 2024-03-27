47a0de848330097ec2c5c505f602fb090e9ac0f0
JIK-K <dnswlrsla@gmail.com>
2024-03-27 19:47:31 +0900

    feat: update namespace by memberName

--- src/socket/socket.gateway.ts
+++ src/socket/socket.gateway.ts
@@ -21,17 +21,28 @@ export default class SocketGateway
   private namespaces: Map<string, Socket[]> = new Map();
   private logger: Logger = new Logger('FileEventsGateway');
 
+  afterInit(server: any) {
+    this.logger.log('Socket DownloadFile server init ✅');
+  }
+
   handleDisconnect(client: any) {
     this.logger.log(`Client Disconnected : ${client.id}`);
   }
+
   handleConnection(client: any, ...args: any[]) {
+    const memberName = client.handshake.query.memberName;
+    const namespace = `member-${memberName}`;
+    console.log(namespace);
+    if (!this.namespaces.has(namespace)) {
+      this.namespaces.set(namespace, []);
+    }
+    this.namespaces.get(namespace).push(client);
+
     this.logger.log(
       `Client Connected : ${client.id} ${client.request.connection.remoteAddress}`,
     );
   }
-  afterInit(server: any) {
-    this.logger.log('Socket DownloadFile server init ✅');
-  }
+
   @SubscribeMessage('message')
   handleMessage(client: any, payload: any): string {
     return 'Hello world!';
